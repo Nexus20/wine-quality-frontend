f8483331920e51cb670c53fb258fb5dc7da06255
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2023-05-26 01:11:57 +0300

    Removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,24 @@ import {Component, OnInit} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 import {Store} from "@ngxs/store";
-import {AuthState} from "./users/states/auth.state";
 import {ProfileState} from "./profile/state/profile.state";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'wine-quality-system-front-end';
+    title = 'wine-quality-system-front-end';
 
-  constructor(private store: Store, private translateService: TranslateService) {
-  }
+    constructor(private store: Store, private translateService: TranslateService) {
+    }
 
-  ngOnInit(): void {
-    this.translateService.setDefaultLang(environment.defaultLocale)
-    this.translateService.addLangs(['en-US', 'uk-UA']);
-    const language = this.store.selectSnapshot(ProfileState.selectLanguage);
-    // const language = 'uk-UA'
-    this.translateService.use(language);
-  }
+    ngOnInit(): void {
+        this.translateService.setDefaultLang(environment.defaultLocale)
+        this.translateService.addLangs(['en-US', 'uk-UA']);
+        const language = this.store.selectSnapshot(ProfileState.selectLanguage);
+        // const language = 'uk-UA'
+        this.translateService.use(language);
+    }
 }
